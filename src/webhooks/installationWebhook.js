97c449f467a45ec7d2ae26229b979c9f78211055
Jordi Noguera <jordinoguera83+github@gmail.com>
2020-02-10 16:41:37 +0100

    Manage user uninstalling app

--- src/webhooks/installationWebhook.js
+++ src/webhooks/installationWebhook.js
@@ -1,14 +1,22 @@
 const db = require('db')
 
-module.exports = async ({ payload }) => {
-  const { installation } = payload
+module.exports = async ({ payload, log }) => {
+  const { installation, action } = payload
   const { account } = installation
 
   const installations = db.collection('installations')
-  await installations.add({
-    installation_id: installation.id,
-    account_id: account.id,
-    name: account.login,
-    created_at: (new Date()).toISOString()
-  })
+
+  log(`${account.login} ${action} installation`)
+
+  if (action === 'created') {
+    await installations.add({
+      installation_id: installation.id,
+      account_id: account.id,
+      name: account.login,
+      created_at: (new Date()).toISOString()
+    })
+  } else if (action === 'deleted') {
+    const result = await installations.where('installation_id', '==', installation.id).get();
+    await result.docs.forEach(doc => doc.ref.delete())
+  }
 }
